30c34eb9f1dcfcb7ec004e65cc5b037012932e2c
elrincondehiro <administracion@elrincondehiro.com>
2024-10-08 19:06:46 +0200

    primer desarrollo

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,15 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, envField } from 'astro/config';
+
+import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [tailwind()],
+  env: {
+    schema:{
+      STRAPI_API_KEY: envField.string({context: 'server', access: 'secret'}),
+      STRAPI_URL: envField.string({context:'server', access: 'secret'})
+    }
+  }
+});
